bcfc9e264e9ae1ca8d0de27f5c4a2decffc773fe
Phillip <Filuperwinsmith@gmail.com>
2019-12-06 20:59:01 +1100

    Update DictionaryExtensions.cs

--- PenguinHelperLibrary/Extension Methods/DictionaryExtensions.cs
+++ PenguinHelperLibrary/Extension Methods/DictionaryExtensions.cs
@@ -1,9 +1,28 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 
 namespace PenguinHelperLibrary.Extension_Methods
 {
-    internal static class DictionaryExtensions
+    /// <summary>
+    ///     Extension methods for type <see cref="IDictionary" />
+    /// </summary>
+    public static class DictionaryExtensions
     {
+        /// <summary>
+        ///     Gets a value with an <see cref="IDictionary{TKey,TValue}" />, given a key. If the key does not exist, the returned
+        ///     value is the default value of <typeparamref name="TValue" />.
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="dictionary">The <see cref="IDictionary" /> value to get a value.</param>
+        /// <param name="key">
+        ///     The <typeparamref name="TKey" /> key value of a <see cref="KeyValuePair{TKey,TValue}" /> within
+        ///     <paramref name="dictionary" />.
+        /// </param>
+        /// <returns>
+        ///     Returns a <typeparamref name="TValue" /> value paired with <paramref name="key" />, if <paramref name="key" />
+        ///     exists. Otherwise, returns the default type of <typeparamref name="TValue" />.
+        /// </returns>
         public static TValue GetValueOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key)
         {
             dictionary.TryGetValue(key, out var result);
