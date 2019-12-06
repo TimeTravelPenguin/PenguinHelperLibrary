0c455e70b854b1339b6fd3f829c32207f4226d5c
Phillip <Filuperwinsmith@gmail.com>
2019-12-06 18:33:33 +1100

    Create EnumExtensionTests.cs

---
+++ PenguinHelperLibrary.Tests/Extension Method Tests/EnumExtensionTests.cs
@@ -0,0 +1,38 @@
+﻿using AllOverIt.Fixture;
+using FluentAssertions;
+using PenguinHelperLibrary.Extension_Methods;
+using Xunit;
+
+namespace PenguinHelperLibrary.Tests.Extension_Method_Tests
+{
+    public class EnumExtensionTests
+    {
+        private enum Numbers
+        {
+            Zero,
+            One,
+            Two,
+            Three,
+            Four,
+            Five,
+            Six,
+            Seven,
+            Eight,
+            Nine,
+            Ten
+        }
+
+        public class EnumToArray : AoiFixtureBase
+        {
+            [Fact]
+            public void EnumToArrayTest()
+            {
+                var counter = 0;
+                foreach (var number in EnumExtensions.EnumToArray<Numbers>())
+                {
+                    number.Should().Be((Numbers) counter++);
+                }
+            }
+        }
+    }
+}
