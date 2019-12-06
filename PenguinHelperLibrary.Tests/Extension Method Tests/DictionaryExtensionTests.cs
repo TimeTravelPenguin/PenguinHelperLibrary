1ed87722bcc07ff33552f13b239bd064115d8f96
Phillip <Filuperwinsmith@gmail.com>
2019-12-07 00:35:40 +1100

    Update DictionaryExtensionTests.cs

--- PenguinHelperLibrary.Tests/Extension Method Tests/DictionaryExtensionTests.cs
+++ PenguinHelperLibrary.Tests/Extension Method Tests/DictionaryExtensionTests.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using AllOverIt.Fixture;
+using FluentAssertions;
 using PenguinHelperLibrary.Extension_Methods;
 using Xunit;
 
@@ -9,21 +11,95 @@ namespace PenguinHelperLibrary.Tests.Extension_Method_Tests
     {
         public class GetValueOrDefault : AoiFixtureBase
         {
+            /// <summary>
+            ///     Tests successful return of preferred default value of a dictionary, requesting only keys that are not contained.
+            /// </summary>
             [Fact]
-            public void GetValueOrDefaultTest()
+            public void GetValueOrDefaultTest_Default()
             {
-                var dict = new Dictionary<int, int>
-                {
-                    [1] = 11,
-                    [2] = 22
-                };
+                var defaultVal = Create<string>().ToArray();
 
-                var result = dict.GetValueOrDefault(3);
+                // Keys not to be contained within dictionary
+                var randKeys = CreateMany<decimal>(1000).ToArray();
+
+                // Dictionary of keys not containing any keys from randKeys
+                var dict = randKeys.ToDictionary(key => CreateExcluding(randKeys),
+                    value => CreateExcluding(defaultVal));
+
+                // Check for non-existing key. Result should be default value.
+                foreach (var key in randKeys)
+                {
+                    dict.GetValueOrDefault(key, defaultVal[0])
+                        .Should()
+                        .Be(defaultVal[0]);
+                }
+            }
+
+            /// <summary>
+            ///     Tests successful return of dictionary value when giving a contained key.
+            /// </summary>
+            [Fact]
+            public void GetValueOrDefaultTest_NoDefault_ContainedKeys()
+            {
+                var dict = Create<Dictionary<decimal, string>>();
+
+                foreach (var keyVal in dict)
+                {
+                    dict.GetValueOrDefault(keyVal.Key)
+                        .Should()
+                        .Be(keyVal.Value);
+                }
+            }
+
+            /// <summary>
+            ///     Tests successful return of default dictionary value when giving a non-contained key.
+            /// </summary>
+            [Fact]
+            public void GetValueOrDefaultTest_NoDefault_NonContainedKeys()
+            {
+                // Keys not to be contained within dictionary
+                var randKeys = CreateMany<decimal>(1000).ToArray();
+
+                // Dictionary of keys not containing any keys from randKeys
+                var dict = randKeys.ToDictionary(key => CreateExcluding(randKeys), value => Create<string>());
+
+                // Check for non-existing key. Result should be default value.
+                foreach (var key in randKeys)
+                {
+                    dict.GetValueOrDefault(key)
+                        .Should()
+                        .Be(default);
+                }
             }
         }
 
         public class AddKeyValuePair : AoiFixtureBase
         {
+            /// <summary>
+            ///     Tests for successful addition of <see cref="KeyValuePair{TKey,TValue}" /> to
+            ///     <see cref="IDictionary{TKey,TValue}" />.
+            /// </summary>
+            [Fact]
+            public void AddKeyValuePairTest()
+            {
+                // Create KeyValuePairs
+                var keyValPairs = CreateMany<KeyValuePair<decimal, string>>(1000);
+                var dict = new Dictionary<decimal, string>();
+
+                // Use extension method to add
+                foreach (var keyValuePair in keyValPairs)
+                {
+                    dict.AddKeyValuePair(keyValuePair);
+                }
+
+                foreach (var keyValPair in keyValPairs)
+                {
+                    // Assert that the dictionary contains the Key and correct value
+                    dict[keyValPair.Key]
+                        .Should()
+                        .Be(keyValPair.Value);
+                }
+            }
         }
     }
 }
