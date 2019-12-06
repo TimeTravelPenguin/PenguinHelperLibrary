ce45c50cc3c8c57eca5705bf3c27f6a261856980
Phillip <Filuperwinsmith@gmail.com>
2019-12-06 18:33:31 +1100

    Create DictionaryExtensionTests.cs

---
+++ PenguinHelperLibrary.Tests/Extension Method Tests/DictionaryExtensionTests.cs
@@ -0,0 +1,15 @@
+﻿using AllOverIt.Fixture;
+
+namespace PenguinHelperLibrary.Tests.Extension_Method_Tests
+{
+    public class DictionaryExtensionTests
+    {
+        public class GetValueOrDefault : AoiFixtureBase
+        {
+        }
+
+        public class AddKeyValuePair : AoiFixtureBase
+        {
+        }
+    }
+}
