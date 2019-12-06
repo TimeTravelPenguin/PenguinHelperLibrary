6752efa7804e425eae74210c49b3a7dea235b3db
Phillip <Filuperwinsmith@gmail.com>
2019-12-06 20:58:43 +1100

    Update DictionaryExtensionTests.cs

--- PenguinHelperLibrary.Tests/Extension Method Tests/DictionaryExtensionTests.cs
+++ PenguinHelperLibrary.Tests/Extension Method Tests/DictionaryExtensionTests.cs
@@ -1,4 +1,7 @@
-﻿using AllOverIt.Fixture;
+﻿using System.Collections.Generic;
+using AllOverIt.Fixture;
+using PenguinHelperLibrary.Extension_Methods;
+using Xunit;
 
 namespace PenguinHelperLibrary.Tests.Extension_Method_Tests
 {
@@ -6,6 +9,17 @@ namespace PenguinHelperLibrary.Tests.Extension_Method_Tests
     {
         public class GetValueOrDefault : AoiFixtureBase
         {
+            [Fact]
+            public void GetValueOrDefaultTest()
+            {
+                var dict = new Dictionary<int, int>
+                {
+                    [1] = 11,
+                    [2] = 22
+                };
+
+                var result = dict.GetValueOrDefault(3);
+            }
         }
 
         public class AddKeyValuePair : AoiFixtureBase
