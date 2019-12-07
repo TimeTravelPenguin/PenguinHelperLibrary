7dd693b8ec12e3d341319573aeda014091e7fe10
Phillip <Filuperwinsmith@gmail.com>
2019-12-07 12:17:46 +1100

    Create NewtonsoftExtensionTests.cs

---
+++ PenguinHelperLibrary.Tests/Extension Method Tests/NewtonsoftExtensionTests.cs
@@ -0,0 +1,80 @@
+﻿#region Title Header
+
+// Name: Phillip Smith
+// 
+// Solution: PenguinHelperLibrary
+// Project: PenguinHelperLibrary.Tests
+// File Name: NewtonsoftExtensionTests.cs
+// 
+// Current Data:
+// 2019-12-07 11:45 AM
+// 
+// Creation Date:
+// 2019-12-07 11:14 AM
+
+#endregion
+
+using System;
+using AllOverIt.Fixture;
+using FluentAssertions;
+using JetBrains.Annotations;
+using Newtonsoft.Json;
+using PenguinHelperLibrary.Extension_Methods;
+using Xunit;
+
+namespace PenguinHelperLibrary.Tests.Extension_Method_Tests
+{
+    [UsedImplicitly]
+    public class NewtonsoftExtensionTests
+    {
+        /// <summary>
+        ///     Object used to test serialization
+        /// </summary>
+        [UsedImplicitly]
+        internal class RandomObject
+        {
+            public string RandomString { get; set; }
+            public int RandomInt { get; set; }
+            public double RandomDouble { get; set; }
+            public DateTime RandomDateTime { get; set; }
+        }
+
+        /// <summary>
+        ///     Tests for serialization
+        /// </summary>
+        public class SerializeObjectTests : AoiFixtureBase
+        {
+            /// <summary>
+            ///     Tests that the serialization extension method returns the correct result
+            /// </summary>
+            [Fact]
+            public void SerializeObjectTest()
+            {
+                var randomObject = Create<RandomObject>();
+
+                randomObject.SerializeObject()
+                    .Should()
+                    .Be(JsonConvert.SerializeObject(randomObject));
+            }
+        }
+
+        /// <summary>
+        ///     Tests for deserialization
+        /// </summary>
+        public class DeserializeObjectTests : AoiFixtureBase
+        {
+            /// <summary>
+            ///     Tests that the deserialization extension method returns the correct result
+            /// </summary>
+            [Fact]
+            public void DeserializeObjectTest()
+            {
+                var randomSerialization = JsonConvert.SerializeObject(Create<RandomObject>());
+
+                randomSerialization.DeserializeObject<RandomObject>()
+                    .Should()
+                    .BeEquivalentTo(JsonConvert.DeserializeObject<RandomObject>(randomSerialization));
+            }
+        }
+    }
+}
