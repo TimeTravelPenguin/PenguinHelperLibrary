2e64f623f7033f2ace6984924ac9c7751fa62faf
Phillip <Filuperwinsmith@gmail.com>
2019-12-08 01:18:48 +1100

    Update Point2D.cs

--- PenguinHelperLibrary/Objects/Point2D.cs
+++ PenguinHelperLibrary/Objects/Point2D.cs
@@ -18,13 +18,29 @@ namespace PenguinHelperLibrary.Objects
 {
     public class Point2D
     {
+        /// <summary>
+        /// The X coordinate of the point
+        /// </summary>
         public double X { get; set; }
+
+        /// <summary>
+        /// The Y coordinate of the point
+        /// </summary>
         public double Y { get; set; }
 
+        /// <summary>
+        /// Returns a <see cref="Point2D"/> with <see langword="X = 0"/>, <see langword="Y = 0"/>
+        /// </summary>
         public static Point2D Origin => new Point2D(0, 0);
 
-        public Point2D() : this(0, 0)
+        public Point2D() : this(Origin)
         {
+        }
+
+        public Point2D(Point2D point)
+        {
+            X = point.X;
+            Y = point.Y;
         }
 
         public Point2D(double x, double y)
