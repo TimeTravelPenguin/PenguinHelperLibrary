6a3b573210dc718f0c5728fb73a8bc695ee0e187
Phillip <Filuperwinsmith@gmail.com>
2019-12-09 01:40:46 +1100

    Create Point.cs

---
+++ PenguinHelperLibrary/Objects/Point.cs
@@ -0,0 +1,23 @@
+﻿#region Title Header
+
+// Name: Phillip Smith
+// 
+// Solution: PenguinHelperLibrary
+// Project: PenguinHelperLibrary
+// File Name: Point.cs
+// 
+// Current Data:
+// 2019-12-09 1:05 AM
+// 
+// Creation Date:
+// 2019-12-09 12:29 AM
+
+#endregion
+
+namespace PenguinHelperLibrary.Objects
+{
+    public class Point
+    {
+        public double Value { get; set; }
+    }
+}
