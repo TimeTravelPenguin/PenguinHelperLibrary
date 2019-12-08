3e3ddad0a460a41632c767f1911df1514f1511c2
Phillip <Filuperwinsmith@gmail.com>
2019-12-09 01:45:48 +1100

    Update ArrayExtensionMethods.cs

--- PenguinHelperLibrary/Extension Methods/ArrayExtensionMethods.cs
+++ PenguinHelperLibrary/Extension Methods/ArrayExtensionMethods.cs
@@ -7,7 +7,7 @@
 // File Name: ArrayExtensionMethods.cs
 // 
 // Current Data:
-// 2019-12-09 1:09 AM
+// 2019-12-09 1:45 AM
 // 
 // Creation Date:
 // 2019-12-09 1:02 AM
@@ -18,28 +18,46 @@ using System;
 
 namespace PenguinHelperLibrary.Extension_Methods
 {
+    /// <summary>
+    ///     Extension methods for <see cref="Array" /> objects
+    /// </summary>
     public static class ArrayExtensionMethods
     {
+        /// <summary>
+        ///     Fills an array with an object or value. If an object, it will be the same reference for each.
+        /// </summary>
+        /// <typeparam name="T">
+        ///     The type of the array
+        /// </typeparam>
+        /// <param name="arr">
+        ///     The array to fill
+        /// </param>
+        /// <param name="value">
+        ///     The value to fill
+        /// </param>
         public static void Fill<T>(this T[] arr, T value)
         {
-            if (arr == null)
-            {
-                throw new NullReferenceException(nameof(arr));
-            }
-
             for (var i = 0; i < arr.Length; i++)
             {
                 arr[i] = value;
             }
         }
 
+        /// <summary>
+        ///     Fills an array with an object or value for any null objects in the array.
+        ///     If an object, it will be the same reference for each.
+        /// </summary>
+        /// <typeparam name="T">
+        ///     The type of the array
+        /// </typeparam>
+        /// <param name="arr">
+        ///     The array to fill
+        /// </param>
+        /// <param name="value">
+        ///     The value to fill
+        /// </param>
         public static void FillIfNull<T>(this T[] arr, T value)
         {
-            if (arr == null)
-            {
-                throw new NullReferenceException(nameof(arr));
-            }
-
             for (var i = 0; i < arr.Length; i++)
             {
                 if (arr[i] == null)
