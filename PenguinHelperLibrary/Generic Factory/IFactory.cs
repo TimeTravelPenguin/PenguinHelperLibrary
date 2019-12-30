f21e8eaeddc1bdc59f65f9b777ed345f02ec64fe
Phillip Smith <FilupErwinSmith@gmail.com>
2019-12-30 18:14:58 +1100

    Added Generic Factory

---
+++ PenguinHelperLibrary/Generic Factory/IFactory.cs
@@ -0,0 +1,26 @@
+﻿#region Title Header
+
+// Name: Phillip Smith
+// 
+// Solution: PenguinHelperLibrary
+// Project: PenguinHelperLibrary
+// File Name: IFactory.cs
+// 
+// Current Data:
+// 2019-12-30 4:40 PM
+// 
+// Creation Date:
+// 2019-12-30 4:40 PM
+
+#endregion
+
+using System;
+
+namespace PenguinHelperLibrary.Generic_Factory
+{
+  internal interface IFactory<T> where T : class
+  {
+    T Create(string key);
+    void Register(string key, Func<T> valueFunc);
+  }
+}
