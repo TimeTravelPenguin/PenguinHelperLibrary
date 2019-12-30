cb56766aca5845598cb34761f3d4aae73f9a2fc1
Phillip Smith <FilupErwinSmith@gmail.com>
2019-12-30 18:37:20 +1100

    Made public and added xml docs

--- PenguinHelperLibrary/Generic Factory/IFactory.cs
+++ PenguinHelperLibrary/Generic Factory/IFactory.cs
@@ -7,10 +7,10 @@
 // File Name: IFactory.cs
 // 
 // Current Data:
-// 2019-12-30 4:40 PM
+// 2019-12-30 6:32 PM
 // 
 // Creation Date:
-// 2019-12-30 4:40 PM
+// 2019-12-30 6:17 PM
 
 #endregion
 
@@ -18,9 +18,35 @@ using System;
 
 namespace PenguinHelperLibrary.Generic_Factory
 {
-  internal interface IFactory<T> where T : class
+  /// <summary>
+  ///   Interface for generic factory.
+  /// </summary>
+  /// <typeparam name="T">
+  ///   The type of object factory creates.
+  /// </typeparam>
+  public interface IFactory<T> where T : class
   {
+    /// <summary>
+    ///   Create a new object, given by <paramref name="key" />.
+    /// </summary>
+    /// <param name="key">
+    ///   The key given to create an object.
+    /// </param>
+    /// <returns>
+    ///   Returns object of type <typeparamref name="T" />.
+    /// </returns>
     T Create(string key);
+
+    /// <summary>
+    ///   Registers a new object to the factory registry so that the factory can then create a new object that didn't
+    ///   previously exist within the registry.
+    /// </summary>
+    /// <param name="key">
+    ///   The key to be assigned to the registry.
+    /// </param>
+    /// <param name="valueFunc">
+    ///   The <see cref="Func{T1}" /> to generate a new object for <paramref name="key" />.
+    /// </param>
     void Register(string key, Func<T> valueFunc);
   }
 }
