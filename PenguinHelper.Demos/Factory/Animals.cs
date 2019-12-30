470b723630c8f0da6773b6283c7d125bb99782cf
Phillip Smith <FilupErwinSmith@gmail.com>
2019-12-30 23:49:12 +1100

    Changed demo to internal

--- PenguinHelper.Demos/Factory/Animals.cs
+++ PenguinHelper.Demos/Factory/Animals.cs
@@ -1,9 +1,25 @@
-﻿namespace PenguinHelperLibrary.Demos.Factory
+﻿#region Title Header
+
+// Name: Phillip Smith
+// 
+// Solution: PenguinHelperLibrary
+// Project: PenguinHelperLibrary.Demos
+// File Name: Animals.cs
+// 
+// Current Data:
+// 2019-12-30 11:16 PM
+// 
+// Creation Date:
+// 2019-12-30 6:17 PM
+
+#endregion
+
+namespace PenguinHelperLibrary.Demos.Factory
 {
   /// <summary>
-  /// Default factory registry
+  ///   Default factory registry
   /// </summary>
-  public static class Animals
+  internal static class Animals
   {
     public const string Dog = "Dog";
     public const string Cat = "Cat";
